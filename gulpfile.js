1492d2bd8111efe7db7cb698540a760976d84fa2
tsvetkovpro <285019@gmail.com>
2015-08-11 16:12:56 +0500

    rewrite gul.js
    gulp build and gul dev in one gulp.js file

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,14 @@ var gulp     = require('gulp'),
 	livereload = require('gulp-livereload'),
 	connect    = require('gulp-connect'),
 	sass       = require('gulp-sass'),
-	wiredep    = require('wiredep').stream;
+	wiredep    = require('wiredep').stream,
+    useref     = require('gulp-useref'),
+    gulpif     = require('gulp-if'),
+    uglify     = require('gulp-uglify'),
+    minifyCss  = require('gulp-minify-css'),
+    clean      = require('gulp-clean'),
+    gutil      = require( 'gulp-util' ),
+    ftp        = require( 'vinyl-ftp' );
 
 
 //server connect
@@ -33,6 +40,34 @@ gulp.task('html', function () {
 	.pipe(connect.reload());
 	})
 
+
+//bower
+gulp.task('bower', function () {
+  gulp.src('app/index.html')
+    .pipe(wiredep({
+      directory : "app/components/"
+    }))
+    .pipe(gulp.dest('app/'));
+});
+
+//clean
+gulp.task('clean', function () {
+    return gulp.src('build/', {read: false})
+        .pipe(clean());
+    });
+
+//build
+gulp.task('build', ['clean'], function () {
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('build'));
+});
 
 //bower
 gulp.task('bower', function () {
